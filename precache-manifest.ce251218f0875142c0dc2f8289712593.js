300978aee51eddd8cc3bffb55f75496b070d4691
Daniel Rooein <Daniel.Rooein@gmail.com>
2020-03-29 17:32:43 -0400

    Updates

---
+++ precache-manifest.ce251218f0875142c0dc2f8289712593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56b82e18e2b012fa311ea241faa062d",
+    "url": "/react-mosters-rolodex/index.html"
+  },
+  {
+    "revision": "39335630b101ccf1dbf4",
+    "url": "/react-mosters-rolodex/static/css/main.38f1868a.chunk.css"
+  },
+  {
+    "revision": "eea39d6ccacff15cd72e",
+    "url": "/react-mosters-rolodex/static/js/2.ebb27119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mosters-rolodex/static/js/2.ebb27119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39335630b101ccf1dbf4",
+    "url": "/react-mosters-rolodex/static/js/main.11a4cf84.chunk.js"
+  },
+  {
+    "revision": "327234b7850ade1ae0b7",
+    "url": "/react-mosters-rolodex/static/js/runtime-main.d80ad1b0.js"
+  }
+]);
